12f929eabaad261362141e84638b4ea9aaebdeb3
akbar_syam <akbar_syam@rudel.jp>
2022-11-17 19:28:58 +0900

    Changed to QuickLoot

--- SamplePlugin/Configuration.cs
+++ SamplePlugin/Configuration.cs
@@ -1,28 +1,19 @@
 ﻿using Dalamud.Configuration;
 using Dalamud.Plugin;
-using System;
+using Newtonsoft.Json;
 
-namespace SamplePlugin
+namespace DalamudPluginProjectTemplate
 {
-    [Serializable]
     public class Configuration : IPluginConfiguration
     {
-        public int Version { get; set; } = 0;
+        public bool EnableChatLogMessage = true;
+        [JsonIgnore]
+        private DalamudPluginInterface pluginInterface;
 
-        public bool SomePropertyToBeSavedAndWithADefault { get; set; } = true;
+        public int Version { get; set; }
 
-        // the below exist just to make saving less cumbersome
-        [NonSerialized]
-        private DalamudPluginInterface? PluginInterface;
+        public void Initialize(DalamudPluginInterface pluginInterface) => this.pluginInterface = pluginInterface;
 
-        public void Initialize(DalamudPluginInterface pluginInterface)
-        {
-            this.PluginInterface = pluginInterface;
-        }
-
-        public void Save()
-        {
-            this.PluginInterface!.SavePluginConfig(this);
-        }
+        public void Save() => pluginInterface.SavePluginConfig(this);
     }
 }
